df89ca754df97d23a8b30d72d63fcf139742b90b
Joshua St.Laurent <jstl35@comcast.net>
2024-04-29 22:58:19 -0400

    finished card component, added array to app

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,28 @@
 import React from "react";
 import classnames from "classnames";
-import "../app/assets";
+import cardBack from "../app/assets/cardBack.png";
 
 const Card = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
   const handleClick = () => {
     !isFlipped && !isDisabled && onClick(index);
   };
+
+  return (
+    <div
+      className={classnames("card", {
+        "is-flipped": isFlipped,
+        "is-inactive": isInactive,
+      })}
+      onClick={handleClick}
+    >
+      <div classname="card-face card-front-face">
+        <img src={cardBack} alt="back of card" />
+      </div>
+      <div classname="card-face card-back-face">
+        <img src={card.image} alt="card front" />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
