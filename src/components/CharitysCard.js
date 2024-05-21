b2f987bd382cd3105935c4327874cd8eb59ad5f0
Charity Foote <footefamily@yahoo.com>
2024-05-21 16:46:01 -0500

    Card flips back over

--- src/components/CharitysCard.js
+++ src/components/CharitysCard.js
@@ -1,27 +1,33 @@
 import { useState } from "react";
 import { useSpring, a } from 'react-spring'
 
-const GameCard = ({ card, handleFlippedCards, stopflip}) => {
+const GameCard = ({ card, handleFlippedCards, stopflip }) => {
     const [flipped, setFlipped] = useState(false);
     const { transform, opacity } = useSpring({
-      opacity: flipped ? 1 : 0,
-      transform: `perspective(600) rotateY(${flipped ? 180 : 0}deg)`,
-      config: { mass: 5, tension: 500, friction: 80 },
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600) rotateY(${flipped ? 180 : 0}deg)`,
+        config: { mass: 5, tension: 500, friction: 80 },
     });
 
     const handleClick = () => {
         setFlipped(state => !state);
         console.log('clicked!');
+        const flipId = setInterval(() => {
+            setFlipped(state => !state);
+            clearInterval(flipId);
+        }, 5000);
+
     }
 
     return (
-        <div className="item" onClick={() => {handleClick(); !stopflip && handleFlippedCards(card)}}>
-            <a.div className="card back" 
-                    style={{ opacity: opacity.to(o => 1 - o), transform}}/>
+        <div className="item" onClick={() => { handleClick(); !stopflip && handleFlippedCards(card) }}>
+            <a.div className="card back"
+                style={{ opacity: opacity.to(o => 1 - o), transform }} />
             <a.div className="card front"
-                    style={{ opacity, transform, rotateY: '180deg',
-                    }}>
-                <img src={card.image} alt={card.name}/>
+                style={{
+                    opacity, transform, rotateY: '180deg',
+                }}>
+                <img src={card.image} alt={card.name} />
             </a.div>
         </div>
     );
